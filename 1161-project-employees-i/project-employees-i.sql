991d6ecab596753738d2d826661d8605d616cb55
shahanaj khan <162014281+sahanajkhan@users.noreply.github.com>
2026-07-21 12:22:21 +0530

    Time: 499 ms (85.61%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -2,9 +2,9 @@
 select p.project_id,
 ROUND(AVG(e.experience_years),2) as average_years
 from 
-Project as p
-join 
-Employee as e ON p.employee_id = e.employee_id
+Project as  p
+join
+Employee  as e on p.employee_id = e.employee_id
 Group by
 p.project_id;
 
