21a5296aa586d14efdb48f5aa6de21cad19e2989
shahanaj khan <162014281+sahanajkhan@users.noreply.github.com>
2026-07-24 10:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (93.36%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-         double result = pow(x,n);
 
-        return result;
+        if(n <0){
+            x = 1 /x;
+        }
+
+        long num = labs(n);
+        double pow = 1;
+
+        while(num){
+            if(num & 1){
+                pow *= x;
+            }
+
+            x *= x;
+            num >>= 1;
+        }
+
+        return pow;
     }
 };
