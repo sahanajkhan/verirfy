5a9eb13c568582a17be2dfa61c8fe6948f3f3466
shahanaj khan <162014281+sahanajkhan@users.noreply.github.com>
2026-07-30 14:57:03 +0530

    Time: 3 ms (69.48%) | Memory: 16.3 MB (15.71%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
+    void solve(int open ,int close , string op , vector<string> &v){
+     if(close == 0 && open == 0){
+        v.push_back(op);
+        return;
 
-    void solve(int open , int close , string op ,vector<string> &v){
-        if(open == 0 && close == 0){
-            v.push_back(op);
-            return;
-        }
-        if(open != 0){
-            string  op1 = op;
-            op1.push_back('(');
-            solve(open - 1, close , op1,v);
-        }
+     }
 
-        if(close > open){
-            string op2 = op;
-            op2.push_back(')');
+     if(open != 0){
+        string op1 = op;
+        op1.push_back('(');
+        solve(open - 1, close ,op1,v);
+     }
 
-            solve(open , close - 1 , op2 ,v);
-        }
+     if(close > open){
+        string op2 = op;
+        op2.push_back(')');
+
+        solve(open , close - 1, op2 ,v);
+     }
+
     }
+
+   
     vector<string> generateParenthesis(int n) {
-        vector<string> v;
+       
+       vector<string> v;
+        string op = "";
+       int open = n;
+       int close = n;
 
-        int close = n;
-        int open = n;
+       solve(open,close,op,v);
 
-         string op = "";
-
-         solve(open , close , op, v);
-         return v;
+       return v;
     }
 };
