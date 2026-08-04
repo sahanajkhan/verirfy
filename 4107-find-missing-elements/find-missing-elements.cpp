1578ad7b3cd2b43e09d24163e157eaed5a5d5014
shahanaj khan <162014281+sahanajkhan@users.noreply.github.com>
2026-08-04 12:18:11 +0530

    Time: 8 ms (12.18%) | Memory: 33.7 MB (28.39%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
         int n =nums.size();
-        sort(nums.begin(), nums.end());
-
+        
         unordered_set<int> mp;
 
         vector<int> arr;
-       int st = nums[0];
-       int end = nums[n-1];
+
+        int st = INT_MAX;
+        int end = INT_MIN;
+       
+
+       for(int i =0; i<n; i++){
+          st = min(st,nums[i]);
+          end = max(end, nums[i]);
+       }
         for(int i=0;i<n;i++){
            mp.insert(nums[i]);
         }
