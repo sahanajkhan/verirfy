10c9c0479a3720ae568fb09aa149ce11a5a9d510
shahanaj khan <162014281+sahanajkhan@users.noreply.github.com>
2026-09-23 03:15:42 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (62.42%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+
+        int n = prices.size();
+        int maxa = 0;
+        sort(prices.begin() , prices.end());
+
+        int sum = prices[0] + prices[1];
+
+        if(sum > money){
+            return money;
+        }
+        else {
+            return abs(money - sum);
+        }
+
+
+return 0;
+        
+    }
+};
